6df533c9783850df730be96f4d595d97a12079ae
bloomHouseMarketing <hello@bloomhousemarketing.com>
2025-10-27 08:57:23 -0700

    project upload

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://navixhealth.com',
+  trailingSlash: 'never',
+  integrations: [
+    sitemap({
+      serialize: (item) => {
+        const url = item.url.endsWith('/') ? item.url.slice(0, -1) : item.url;
+        return { ...item, url };
+      },
+    }),
+  ],
+});
