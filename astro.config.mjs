00b1fb73c45c9a6d1df9ee3d5021b19abcf4319c
bloomHouseMarketing <hello@bloomhousemarketing.com>
2025-11-25 02:35:02 +0500

    redirect links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,11 +8,35 @@ export default defineConfig({
   integrations: [
     sitemap({
       serialize: (item) => {
-        const url = item.url.endsWith('/') ? item.url.slice(0, -1) : item.url;
-        return { ...item, url };
+        const url = item.url.endsWith('/') ? item.url.slice(0, -1) : item.url
+        return { ...item, url }
       },
     }),
   ],
+
+  redirects: {
+    '/news': '/blogs',
+
+    '/news/disrupt-magazine-features-navix-health-inc-the-future-of-behavioral-health-facilities-lies-in-ai':
+      '/blogs/disrupt-magazine-features-navix-health-inc-the-future-of-behavioral-health-facilities-lies-in-ai',
+
+    '/news/unveiling-navixai-tm': '/blogs/unveiling-navixai-tm',
+
+    '/news/navix-healths-innovative-approach-featured-in-msn':
+      '/blogs/navix-healths-innovative-approach-featured-in-msn',
+
+    '/news/new-york-weekly-featured-navix-health-advancing-behavioral-health-software':
+      '/blogs/new-york-weekly-featured-navix-health-advancing-behavioral-health-software',
+
+    '/news/forbes-features-navix-health-the-transformative-power-of-ai-in-behavioral-healthcare':
+      '/blogs/forbes-features-navix-health-the-transformative-power-of-ai-in-behavioral-healthcare',
+
+    '/news/business-insider-features-navix-health-leveraging-ai-to-transform-behavioral-health':
+      '/blogs/business-insider-features-navix-health-leveraging-ai-to-transform-behavioral-health',
+
+    '/news/navix-health-partners-with-sobrsafe-for-advanced-alcohol-monitoring-in-digital-healthcare':
+      '/blogs/navix-health-partners-with-sobrsafe-for-advanced-alcohol-monitoring-in-digital-healthcare',
+  },
 
   server: {
     headers: {
@@ -22,4 +46,4 @@ export default defineConfig({
       'X-Frame-Options': 'SAMEORIGIN',
     },
   },
-});
+})
