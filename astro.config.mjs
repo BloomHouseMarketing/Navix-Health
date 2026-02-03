aedf838a54247bfaeb751af92ad2b10a92a20896
bloomHouseMarketing <hello@bloomhousemarketing.com>
2026-02-03 22:35:19 +0500

    replace Image to img

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://navixhealth.com',
+  base: '/',
   trailingSlash: 'never',
   integrations: [
     sitemap({
